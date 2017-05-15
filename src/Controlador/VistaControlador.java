b2054c30e5f0dde9473eb1b00c74912b0dadc3fb
Sergio Moreno <sergiomoreno@MacBook-Air-de-Sergio.local>
2017-05-15 18:52:56 +0200

    Inicial + imagen

--- src/Controlador/VistaControlador.java
+++ src/Controlador/VistaControlador.java
@@ -1,9 +1,20 @@
 package Controlador;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
 public class VistaControlador {
-
-	public VistaControlador() {
-		// TODO Auto-generated constructor stub
+	/**
+	 * Función que dirige al usuario a la venta principal
+	 * @param goMain recibe elJButton que va a ser clickeado
+	 */
+	public static void goMain(JButton goMain){
+		goMain.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Yendo al main");
+			}
+		});
 	}
-
 }
--- src/Vista/InicioVista.java
+++ src/Vista/InicioVista.java
@@ -3,6 +3,21 @@ package Vista;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import java.awt.Component;
+import javax.swing.Box;
+import java.awt.Font;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+
+import Controlador.VistaControlador;
+
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class InicioVista {
 
@@ -19,9 +34,40 @@ public class InicioVista {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		VistaControlador vc = new VistaControlador();
 		frame = new JFrame();
+		frame.setTitle("MusLearn - DAM");
+		frame.getContentPane().setBackground(new Color(238, 238, 238));
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+		
+		Component verticalStrut = Box.createVerticalStrut(20);
+		frame.getContentPane().add(verticalStrut);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(InicioVista.class.getResource("/Imagen/je.png")));
+		lblNewLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		frame.getContentPane().add(lblNewLabel);
+		
+		Component verticalStrut_1 = Box.createVerticalStrut(20);
+		frame.getContentPane().add(verticalStrut_1);
+		
+		JLabel lblNewLabel_1 = new JLabel("Aplicación que incorpora todas las letras del mercado, y videos..");
+		lblNewLabel_1.setBackground(new Color(192, 192, 192));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setFont(new Font("Kohinoor Bangla", Font.PLAIN, 13));
+		lblNewLabel_1.setAlignmentX(Component.CENTER_ALIGNMENT);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		Component verticalGlue = Box.createVerticalGlue();
+		frame.getContentPane().add(verticalGlue);
+		
+		JButton goMain = new JButton("Ir a la APP");
+		//Método hecho en el controlador de esta vista la cual te lleva a otra.
+		vc.goMain(goMain);
+		goMain.setAlignmentX(Component.CENTER_ALIGNMENT);
+		frame.getContentPane().add(goMain);
 	}
 
 }
